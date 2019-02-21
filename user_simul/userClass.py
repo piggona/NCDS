6b00e0cb8af3ee0e97697d2710b5f6b055b6f733
piggona <2803302887@qq.com>
2019-02-21 11:35:17 +0800

    2019-2-21,添加了代理池及数据库管理

--- user_simul/userClass.py
+++ user_simul/userClass.py
@@ -40,19 +40,20 @@ class userClass:
             user_prob_mode = user_config["user_prob_mode"]
             user_mode_id = random_index(user_prob_mode)
             print(user_mode_id)
-            user_mode = mode_collection.find_one({"mode_id":user_mode_id})
+            user_modes = mode_collection.find({"mode_id":user_mode_id})
 
-            device_prob = user_mode["acting_mode"]["device"]
-            self.device = device_prob["device_type"][random_index(device_prob["prob"])]
-            user_conf = new_user()["data"]
-            self.token = user_conf["token"]
-            self.header = ra_header(self.device,user_conf["token"])
-            user_profile = {"user_id":user_conf["uid"],"mode_id":user_mode_id,"header":self.header}
-            user_collection.insert_one(user_profile)
+            for user_mode in user_modes:
+                device_prob = user_mode["acting_mode"]["device"]
+                self.device = device_prob["device_type"][random_index(device_prob["prob"])]
+                user_conf = new_user()["data"]
+                self.token = user_conf["token"]
+                self.header = ra_header(self.device,user_conf["token"])
+                user_profile = {"user_id":user_conf["uid"],"mode_id":user_mode_id,"header":self.header}
+                user_collection.insert_one(user_profile)
 
-            self.read_preference = user_mode["acting_mode"]["read_preference"]
-            self.user_id = user_conf["uid"]
-            self.mode_id = user_mode_id
+                self.read_preference = user_mode["acting_mode"]["read_preference"]
+                self.user_id = user_conf["uid"]
+                self.mode_id = user_mode_id
         else:
             user = user_collection.find_one({"user_id":user_id})
             self.device = user["header"]["os"]
