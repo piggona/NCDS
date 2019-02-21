4cafe3b99baf16f641e71f8c258e56cc9167356c
piggona <2803302887@qq.com>
2019-02-21 13:38:30 +0800

    2019-2-21,添加了代理池及数据库管理

--- user_simul/userClass.py
+++ user_simul/userClass.py
@@ -75,6 +75,7 @@ class userClass:
             sys_config = json.load(r)
         rec_url = sys_config["api_url"] + sys_config["news_path"]
         print(rec_url)
+        print(self.header)
         data = {
             "type": "1",
             "refresh": "1",
