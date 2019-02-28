9c0035d82e6f05b9cdc6c06a47bff81ae62a1567
piggona <2803302887@qq.com>
2019-02-28 16:35:50 +0800

    ctr analysis

--- ctr_analysis/new_users/new_users.py
+++ ctr_analysis/new_users/new_users.py
@@ -11,15 +11,19 @@ import pymysql
 def fetch_new_user():
     conn = pymysql.connect(host='127.0.0.1',port=3306,user="jinyuanhao",db="infomation",passwd="Sjk0213%$")
     cursor = conn.cursor()
-    cursor.execute("SELECT user_id from aliyun_user_info Where register_time > 1551241093")
-    row = cursor.fetchall()
-    print(row)
-
+    user_query = "SELECT user_id from aliyun_user_info Where register_time >"+str(int(time.time()) - 86400)
+    cursor.execute(user_query)
+    users = cursor.fetchall()
+    user_list = []
+    for user in users:
+        user_list.append(user[0])
     conn.commit()
     cursor.close()
     conn.close()
+    return user_list
 
 
 if __name__ == "__main__":
-    fetch_new_user()
+    user_list = fetch_new_user()
+    print(user_list)
 
