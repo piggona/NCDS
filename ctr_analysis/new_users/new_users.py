f31aadaa3dcb912a13c458892db4c02d0474fd8c
piggona <2803302887@qq.com>
2019-02-28 18:11:56 +0800

    ctr analysis

--- ctr_analysis/new_users/new_users.py
+++ ctr_analysis/new_users/new_users.py
@@ -25,19 +25,51 @@ def fetch_new_user():
 def get_start_ctr():
     '''
     得到每个user_list用户的ctr信息,存储到mongodb中
+    计算用户首刷ctr
+    计算用户首click的expose次数
+    计算用户100次操作ctr
     '''
+    client = pymongo.MongoClient(host="localhost",port=27017)
+    db = client.ctrAnalytics
+    collection = db["start_ctr"]
+
     user_ids = fetch_new_user()
     conn = pymysql.connect(host='127.0.0.1',port=3306,user="jinyuanhao",db="infomation",passwd="Sjk0213%$")
     cursor = conn.cursor()
     for user_id in user_ids:
-        query = "SELECT item_id,bhv_time,user_id,bhv_type FROM aliyun_behavior_info WHERE user_id = {} ORDER BY bhv_time ASC".format(user_id)
-        cursor.execute(query)
+        user_ctr_item = {"user_id":user_id,"user_data":{"first_ctr":"","first_click":"","total_ctr":"","raw_data":""}}
+        first_query = "SELECT item_id,bhv_type FROM aliyun_behavior_info WHERE user_id = {} ORDER BY bhv_time ASC LIMIT 100".format(str(user_id))
+        cursor.execute(first_query)
         user_items = cursor.fetchmany(100)
-        print(user_items)
+        user_ctr_item["user_data"]["raw_data"] = user_items
+        expose_count = 0
+        click_count = 0
+        first_ctr = 0
+        for user_item in user_items:
+            if user_item[1] == "expose":
+                expose_count += 1
+                if (expose_count == 10) and (first_ctr == 0):
+                    user_ctr_item["user_data"]["first_ctr"] = 0
+                    first_ctr = 1
+            if user_item[1] == "click":
+                if first_ctr == 0:
+                    user_ctr_item["user_data"]["first_ctr"] = 1 / expose_count
+                    first_ctr = 1
+                if click_count == 0:
+                    user_ctr_item["user_data"]["first_click"] = expose_count
+                click_count += 1
+        user_ctr_item["user_data"]["total_ctr"] = click_count / expose_count
+        collection.insert_one(user_ctr_item)
     conn.commit()
     cursor.close()
     conn.close()
 
+def ctr_analysis():
+    '''
+    计算所有user_list ctr信息的统计量，包括均值，上分位数，标准差等
+    要判断新用户（新用户未浏览即流失，新用户停留->停留时间）
+    '''
+    pass
 
 
 if __name__ == "__main__":
