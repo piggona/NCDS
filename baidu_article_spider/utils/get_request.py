94bf332c4fcf13a2439956ad42896fc28748c1da
piggona <2803302887@qq.com>
2019-02-28 09:35:53 +0800

    百度爬虫

--- baidu_article_spider/utils/get_request.py
+++ baidu_article_spider/utils/get_request.py
@@ -150,6 +150,7 @@ def ra_header(device, token):
 def get_device():
     device = {"imei":get_imei("android"),"mac":get_mac(),"androidId":"Android"+str(random.randint(0,10000)),"model":"","vendor":"","screenWidth":"","screenHeight":"","osType":1,"osVersion":get_os_version("android"),"deviceType":1}
     device_model,info = get_res_os("android")
+    print(info)
     device["model"] = device_model
     device["vendor"] = info["vendor"]
     device["screenWidth"] = info["screenWidth"]
