cf7f528323e8f2369b5d3e5f9283689a2da1be55
piggona <2803302887@qq.com>
2019-03-01 14:49:58 +0800

    ctr analysis

--- ctr_analysis/new_users/new_users.py
+++ ctr_analysis/new_users/new_users.py
@@ -15,9 +15,7 @@ import pymysql
 def fetch_new_user(start_time,user_time_range):
     conn = pymysql.connect(host='127.0.0.1',port=3306,user="jinyuanhao",db="infomation",passwd="Sjk0213%$")
     cursor = conn.cursor()
-    print(start_time)
-    print(user_time_range)
-    user_query = "SELECT user_id from aliyun_user_info Where register_time > "+str(int(start_time)-int(user_time_range))
+    user_query = "SELECT user_id from aliyun_user_info Where register_time > "+str(start_time-user_time_range)
     print("user_query:{}".format(user_query))
     cursor.execute(user_query)
     users = cursor.fetchall()
@@ -115,7 +113,7 @@ def ctr_analysis():
     user_time_range = ctr_config["user_time_range"]
     start_time = ctr_config["user_start_time"]
     if start_time == "now":
-        start_time == int(time.time())
+        start_time = int(time.time())
     get_start_ctr(start_time,user_time_range)
 
 
