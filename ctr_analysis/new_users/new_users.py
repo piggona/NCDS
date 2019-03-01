533a324b8a7ae64ca2cf4450fd6af11a011a7c5f
piggona <2803302887@qq.com>
2019-03-01 14:39:52 +0800

    ctr analysis

--- ctr_analysis/new_users/new_users.py
+++ ctr_analysis/new_users/new_users.py
@@ -5,14 +5,17 @@
 import os
 import time
 import json
+import csv
+import sys
+import codecs
 
 import pymongo
 import pymysql
 
-def fetch_new_user(user_time_range):
+def fetch_new_user(start_time,user_time_range):
     conn = pymysql.connect(host='127.0.0.1',port=3306,user="jinyuanhao",db="infomation",passwd="Sjk0213%$")
     cursor = conn.cursor()
-    user_query = "SELECT user_id from aliyun_user_info Where register_time > "+str(int(time.time()) - user_time_range)
+    user_query = "SELECT user_id from aliyun_user_info Where register_time > "+str(start_time - user_time_range)
     print("user_query:{}".format(user_query))
     cursor.execute(user_query)
     users = cursor.fetchall()
@@ -25,22 +28,38 @@ def fetch_new_user(user_time_range):
     conn.close()
     return user_list
 
-def get_start_ctr(user_time_range):
+
+def get_start_ctr(start_time,user_time_range):
     '''
     得到每个user_list用户的ctr信息,存储到mongodb中
     计算用户首刷ctr
     计算用户首click的expose次数
     计算用户100次操作ctr
     '''
+    # 配置写入csv文件
+    path = os.getcwd()
+    with open(path+'/config/ctr_config.json','r') as r:
+        ctr_config = json.load(r)
+    csv_path = ctr_config["usr_csv_path"]
+    r.close()
+    csv_doc_name = "usr_ctr_"+str(int(time.time()))+".csv"
+    csvfile = open(csv_path+csv_doc_name,"w",newline='')
+    writer = csv.writer(csvfile,delimiter='\t',quoting=csv.QUOTE_ALL)
+    csv_head = ["user_id","first_ctr","first_click","total_ctr"]
+    writer.writerow(csv_head)
+
+    #配置写入mongoDB
     client = pymongo.MongoClient(host="localhost",port=27017)
     db = client.ctrAnalytics
     collection = db["start_ctr"]
-    user_ids = fetch_new_user(user_time_range)
-    print("user_ids:{}".format(user_ids))
+    collection.drop()
+
+    user_ids = fetch_new_user(start_time,user_time_range)
+    # print("user_ids:{}".format(user_ids))
     conn = pymysql.connect(host='127.0.0.1',port=3306,user="jinyuanhao",db="infomation",passwd="Sjk0213%$")
     cursor = conn.cursor()
     for user_id in user_ids:
-        print(user_id)
+        # print(user_id)
         user_ctr_item = {"user_id":user_id,"user_data":{"first_ctr":"","first_click":"","total_ctr":"","raw_data":""}}
         first_query = "SELECT item_id,bhv_type FROM aliyun_behavior_info WHERE user_id = {} ORDER BY bhv_time ASC LIMIT 100".format("'"+str(user_id)+"'")
         cursor.execute(first_query)
@@ -68,11 +87,17 @@ def get_start_ctr(user_time_range):
         if expose_count == 0:
             user_ctr_item["user_data"]["total_ctr"] = 0
         else:
+            if click_count == 0:
+                user_ctr_item["user_data"]["first_ctr"] = 0
+                user_ctr_item["user_data"]["first_click"] = 0
             user_ctr_item["user_data"]["total_ctr"] = click_count / expose_count
+        csv_row = [user_ctr_item["user_id"],user_ctr_item["user_data"]["first_ctr"],user_ctr_item["user_data"]["first_click"],user_ctr_item["user_data"]["total_ctr"]]
+        writer.writerow(csv_row)
         collection.insert_one(user_ctr_item)
     conn.commit()
     cursor.close()
     conn.close()
+    csvfile.close()
 
 def ctr_analysis():
     '''
@@ -86,7 +111,13 @@ def ctr_analysis():
     with open(path+"/config/ctr_config.json","r") as r:
         ctr_config = json.load(r)
     user_time_range = ctr_config["user_time_range"]
-    get_start_ctr(user_time_range)
+    start_time = ctr_config["user_start_time"]
+    if start_time == "now":
+        start_time == int(time.time())
+    get_start_ctr(start_time,user_time_range)
+
+
+
 
 def article_ctr_analysis():
     '''
@@ -106,5 +137,5 @@ def data_flow_analysis():
 
 
 if __name__ == "__main__":
-    get_start_ctr(259200)
+    get_start_ctr(int(time.time()),259200)
 
