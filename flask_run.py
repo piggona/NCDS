c64c9082259e1b74fdddf63717c406c0ef415ad7
piggona <2803302887@qq.com>
2019-03-26 11:37:48 +0800

    manual startup system

--- flask_run.py
+++ flask_run.py
@@ -59,8 +59,8 @@ def push_data():
     conn = pymysql.connect(host='rm-2zeg7277v9fkmj3bi.mysql.rds.aliyuncs.com',
                            port=3306, user="information", db="infomation", passwd="Infor0110")
     cursor = conn.cursor()
-    query = "INSERT INTO mine_umeng_push (item_id,title,url,type,push_time,status,created_at,updated_at) VALUES ({0},'{1}','{2}',{3},'{4}',{5},'{6}','{7}')".format(
-        item_id, title, url, 3, push_time,2, create_time, update_time)
+    query = "INSERT INTO mine_umeng_push (item_id,title,url,type,push_time,status,created_at,updated_at) VALUES ({0},'{1}','{2}',{3},'{4}',{5},{6},{7})".format(
+        item_id, title, url, 3, push_time,2, 'NOW()', 'NOW()')
     print(query)
     try:
         cursor.execute(query)
