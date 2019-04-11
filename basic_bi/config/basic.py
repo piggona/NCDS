e17c67bc7f1fccfca8ba09e1e9a6dc3b9bea3a7b
piggona <2803302887@qq.com>
2019-04-11 15:09:27 +0800

    ctr analysis

--- basic_bi/config/basic.py
+++ basic_bi/config/basic.py
@@ -15,6 +15,6 @@ TMP_DB_PSWD = "Sjfy0114!!"
 
 ONLINE_DB = 'infomation'
 ONLINE_DB_HOST = 'rm-2zeg7277v9fkmj3bi.mysql.rds.aliyuncs.com'
-ONLINE_DB_PORT = '3306'
+ONLINE_DB_PORT = 3306
 ONLINE_DB_USER = 'information'
 ONLINE_DB_PSWD = 'Infor0110'
